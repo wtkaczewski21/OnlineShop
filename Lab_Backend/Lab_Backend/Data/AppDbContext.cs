9b5df2f3ada51c4325468da4d146e9559d2d820e
nejski <wojtalokt@gmail.com>
2022-06-24 11:14:12 +0200

    Database connection

---
+++ Lab_Backend/Lab_Backend/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Lab_Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab_Backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                    .Property(p => p.Price)
+                    .HasColumnType("decimal(10,2)");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+    }
+}
