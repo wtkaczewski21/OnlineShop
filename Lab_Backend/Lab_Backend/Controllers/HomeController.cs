b4b720c0f786dff5f5df2241d7ac33d296a5cd7e
nejski <wojtalokt@gmail.com>
2022-06-24 12:21:37 +0200

    Add home controller

--- Lab_Backend/Lab_Backend/Controllers/HomeController.cs
+++ Lab_Backend/Lab_Backend/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using Lab_Backend.Models;
+﻿using Lab_Backend.Data;
+using Lab_Backend.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,9 +13,14 @@ namespace Lab_Backend.Controllers
 {
     public class HomeController : Controller
     {
+        private AppDbContext _db;
+        public HomeController(AppDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_db.Products.Include(c => c.ProductBrand).ToList());
         }
     }
 }
